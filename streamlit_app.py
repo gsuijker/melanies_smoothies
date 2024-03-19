0c256c8b6bc900c02da262f3738671724d3a2989
gsuijker <57193769+gsuijker@users.noreply.github.com>
2024-03-19 16:44:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+    # st.write(ingredients_list)
+    # st.text(ingredients_list)
 
     ingredients_string = ''
 
